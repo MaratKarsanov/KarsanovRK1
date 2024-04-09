39a80620f0e7a4f0113ec4d37b709c293ff6effc
MaratKarsanov <marat.karsanov@mail.ru>
2024-04-09 23:20:20 +0300

    рьпарь

--- KarsanovRK1/Models/Tour.cs
+++ KarsanovRK1/Models/Tour.cs
@@ -1,12 +1,21 @@
-﻿namespace KarsanovRK1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KarsanovRK1.Models
 {
     public class Tour : RepositoryItem
     {
+        [Required(ErrorMessage = "Не указан гид")]
         public string Guide { get; set; }
+
+        [Required(ErrorMessage = "Не указан маршрут")]
         public string Route { get; set; }
         public bool RegistrationIsOpen { get; set; }
         public TourDifficulty Difficulty { get; set; }
+
+        [Required(ErrorMessage = "Не указана дата старта")]
         public DateTime DepartureDate { get; set; }
+
+        [Required(ErrorMessage = "Не указано место отправления")]
         public string GatheringPlace { get; set; }
 
         public Tour()
